3dc33f2fb1d71a6b2f802a336db6db74b974696a
Anonymous <noreply@weblate.org>
2023-06-26 10:59:16 +0000

    Translated using Weblate (English (United States))
    Currently translated at 0.0% (0 of 4 strings)
    
    Added translation using Weblate (English (United States))
    
    Added translation using Weblate (Dutch)
    
    Added translation using Weblate (Indonesian)
    
    Added translation using Weblate (Czech)
    
    Added translation using Weblate (Russian)
    
    Co-authored-by: Anonymous <noreply@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/harbour-file-browser/opal-linkhandler/en_US/
    Translation: Apps by ichthyosaurus for SailfishOS/Opal: Module “Opal.LinkHandler”

---
+++ translations/opal-linkhandler-en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Opal.LinkHandler</name>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="27"/>
+        <source>External Link</source>
+        <translation type="unfinished">External Link</translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="58"/>
+        <source>Open in browser</source>
+        <translation type="unfinished">Open in browser</translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="68"/>
+        <source>Copied to clipboard: %1</source>
+        <translation type="unfinished">Copied to clipboard: %1</translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="77"/>
+        <source>Copy to clipboard</source>
+        <translation type="unfinished">Copy to clipboard</translation>
+    </message>
+</context>
+</TS>
