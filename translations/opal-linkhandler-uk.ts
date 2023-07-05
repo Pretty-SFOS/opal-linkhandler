f8f760d5264b2997f0c89ffbb7186ee7e529a0ae
Dan <jonweblin2205@protonmail.com>
2023-07-05 18:51:21 +0200

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Co-authored-by: Dan <jonweblin2205@protonmail.com>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-linkhandler/uk/
    Translation: Opal for SailfishOS/Module: Opal.LinkHandler

--- translations/opal-linkhandler-uk.ts
+++ translations/opal-linkhandler-uk.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="60"/>
         <source>Open externally</source>
-        <translation type="unfinished"></translation>
+        <translation>Відкрити зовні</translation>
     </message>
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="70"/>
