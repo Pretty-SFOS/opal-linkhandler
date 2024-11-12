8debff3f354773f14fd0372562cb4661a3ba0602
gallegonovato <fran-carro@hotmail.es>
2024-11-12 21:00:33 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-linkhandler/es/
    Translation: Opal for SailfishOS/Module: Opal.LinkHandler

--- translations/opal-linkhandler-es.ts
+++ translations/opal-linkhandler-es.ts
@@ -11,12 +11,12 @@
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="103"/>
         <source>Share</source>
-        <translation type="unfinished"></translation>
+        <translation>Compartir</translation>
     </message>
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="80"/>
         <source>Copy text to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Copiar texto al portapapeles</translation>
     </message>
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="118"/>
@@ -36,7 +36,7 @@
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="21"/>
         <source>Share link</source>
-        <translation type="unfinished"></translation>
+        <translation>Compartir enlace</translation>
     </message>
     <message>
         <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="91"/>
