f802a2888c910a0bd71edaffe27874c48d6ddb94
Languages add-on <noreply-addon-languages@weblate.org>
2025-07-11 11:28:29 +0200

    Added translation using Weblate (Arabic)
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>

---
+++ translations/opal-linkhandler-ar.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>Opal.LinkHandler</name>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="44"/>
+        <source>External Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="103"/>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="80"/>
+        <source>Copy text to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="118"/>
+        <source>Open in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="119"/>
+        <source>Open externally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="26"/>
+        <source>Copied to clipboard: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="21"/>
+        <source>Share link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="91"/>
+        <source>Copy to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
