e8f3992d6189b790b919b87ec63cd34fdede4373
Radio <atoung286@gmail.com>
2026-01-25 23:23:29 +0100

    Added translation using Weblate (Burmese)
    Co-authored-by: Radio <atoung286@gmail.com>

---
+++ translations/opal-linkhandler-my.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="my">
+<context>
+    <name>ExternalUrlPage</name>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="219"/>
+        <source>Swipe left to preview.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="220"/>
+        <source>You are using a mobile data connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Opal.LinkHandler</name>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="137"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="138"/>
+        <source>External link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="175"/>
+        <source>Copy link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="180"/>
+        <source>Copy text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="190"/>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="209"/>
+        <source>Open in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="210"/>
+        <source>Open externally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="95"/>
+        <source>Copied to clipboard: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/ExternalUrlPage.qml" line="122"/>
+        <source>Share link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/PreviewPage.qml" line="58"/>
+        <source>No preview available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/LinkHandler/private/PreviewPage.qml" line="59"/>
+        <source>The page is taking too long to load.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
